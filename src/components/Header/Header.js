702261dbdbc913a32bfe40402eca3bf735d69e8d
mohiyuddin <mohiyuddin.shaikh46@gmail.com>
2022-12-25 15:48:27 +0530

    logo added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BRANDNAME } from "../../constants";
 import "./header.css";
-// import headerLogo from "../../assets/images/escube-header-logo.png";
+import headerLogo from "../../assets/images/plain.png";
+import LogoSVG from "../../assets/icons/LogoSVG";
 
 export default function Header(props) {
   const { isScrolled } = props;
@@ -17,18 +18,7 @@ export default function Header(props) {
     >
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+          <LogoSVG />
           <span class="ml-3 text-xl">{BRANDNAME}</span>
         </a>
         <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center menu-options">
